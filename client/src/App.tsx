a256c57a819acb4a210c9770a88bdaf7df75e55d
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-03-12 22:34:43 -0400

    Add AppBar, turn App into functional component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,34 +3,54 @@ import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 import logo from './logo.svg';
 import './App.css';
+import { Typography, Button, AppBar, Toolbar, IconButton, createStyles, withStyles } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class App extends Component {
-  private client = new ApolloClient({
-    uri: process.env.SERVER_URL || 'http://server:8080'
-  });
+const client = new ApolloClient({
+  uri: process.env.SERVER_URL || 'http://server:8080'
+});
 
-  render() {
-    return (
-      <ApolloProvider client={this.client}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-          </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+const styles = createStyles({
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+});
 
-            </a>
-          </header>
-        </div>
-      </ApolloProvider>
-    );
-  }
-}
-
-export default App;
+const App = ({classes}: any) => (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.grow} variant="h6" color="inherit">
+            Downtime
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  </ApolloProvider>
+);
+export default withStyles(styles)(App);
