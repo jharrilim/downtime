cf943a283309dd5fb9e2b517226dc509b1b28a5b
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-03-12 20:43:15 -0400

    Add apollo client and dotenv, enable service worker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  private client = new ApolloClient({
+    uri: process.env.SERVER_URL || 'http://server:8080'
+  });
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <ApolloProvider client={this.client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+
+            </a>
+          </header>
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- client/src/serviceWorker.ts
+++ client/src/serviceWorker.ts
@@ -20,7 +20,7 @@ const isLocalhost = Boolean(
     )
 );
 
-export function register(config: any) {
+export function register(config?: any) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
