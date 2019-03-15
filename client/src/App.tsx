29495737dde0afa46c9caecc739b4998be70e2af
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-03-14 21:31:14 -0400

    Add drawer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,12 @@ import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 import logo from './logo.svg';
 import './App.css';
-import { Typography, Button, AppBar, Toolbar, IconButton, createStyles, withStyles } from '@material-ui/core';
+import { Typography, Button, AppBar, Toolbar, IconButton, createStyles, withStyles, Drawer, List, ListItem, ListItemIcon, ListItemText, Divider } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import InboxIcon from '@material-ui/icons/Inbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
 
 const client = new ApolloClient({
   uri: process.env.SERVER_URL || 'http://server:8080'
@@ -12,7 +16,15 @@ const client = new ApolloClient({
 
 const styles = createStyles({
   root: {
+    display: 'flex',
     flexGrow: 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
   },
   grow: {
     flexGrow: 1,
@@ -28,7 +40,9 @@ const App = ({classes}: any) => (
     <div className="App">
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu" onClick={() => {
+            console.log('clicked');
+          }}>
             <MenuIcon />
           </IconButton>
           <Typography className={classes.grow} variant="h6" color="inherit">
@@ -37,6 +51,32 @@ const App = ({classes}: any) => (
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
