e755e5553e7431966ccbde0277f0a8189e8f388f
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-03-15 20:54:16 -0400

    Change styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,8 @@ import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 import logo from './logo.svg';
 import './App.css';
-import { Typography, Button, AppBar, Toolbar, IconButton, createStyles, withStyles, Drawer, List, ListItem, ListItemIcon, ListItemText, Divider } from '@material-ui/core';
+import { Typography, Button, AppBar, Toolbar, IconButton, createStyles, withStyles, Drawer, List, ListItem, ListItemIcon, ListItemText, Divider, CssBaseline } from '@material-ui/core';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import MenuIcon from '@material-ui/icons/Menu';
 import InboxIcon from '@material-ui/icons/Inbox';
 import MailIcon from '@material-ui/icons/Mail';
@@ -14,10 +15,12 @@ const client = new ApolloClient({
   uri: process.env.SERVER_URL || 'http://server:8080'
 });
 
-const styles = createStyles({
+const styles = (theme: Theme) => ({
   root: {
     display: 'flex',
-    flexGrow: 1,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
   },
   drawer: {
     width: drawerWidth,
@@ -26,19 +29,18 @@ const styles = createStyles({
   drawerPaper: {
     width: drawerWidth,
   },
-  grow: {
+  content: {
     flexGrow: 1,
+    padding: theme.spacing.unit * 3,
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+  toolbar: theme.mixins.toolbar,
 });
 
 const App = ({classes}: any) => (
   <ApolloProvider client={client}>
     <div className="App">
-      <AppBar position="static">
+    <CssBaseline />
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar>
           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu" onClick={() => {
             console.log('clicked');
@@ -77,19 +79,32 @@ const App = ({classes}: any) => (
           ))}
         </List>
       </Drawer>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
     </div>
   </ApolloProvider>
 );
