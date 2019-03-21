d269a2ecc738fd8933c9fc777b1376b9c4a00429
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2019-03-20 21:43:59 -0400

    Enable mongo usage

--- server/src/data/connection.ts
+++ server/src/data/connection.ts
@@ -1,19 +1,38 @@
-import { createConnection, Connection, getRepository } from 'typeorm';
+import { createConnection, Connection, getRepository, ConnectionOptions } from 'typeorm';
 import entities from './entities';
 import { User } from './entities/user';
 
 export async function connect(): Promise<Connection> {
+    
+    if (process.env.DB_TYPE === 'mongodb') {
+        return await createConnection({
+            type: 'mongodb',
+            database: process.env.DB_NAME || 'downtime',
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            port: +(process.env.DB_PORT || 27017),
+            host: process.env.DB_HOST || 'mongodb',
+            entities: entities,
+            synchronize: true,
+            logger: 'advanced-console',
+            logging: 'all',
+            dropSchema: true,
+            cache: true,
+    
+        });
+    }
+
     return await createConnection({
-        type: "postgres",
-        database: process.env.DB_USER || "admin",
-        username: process.env.DB_USER || "admin",
-        password: process.env.DB_PASSWORD || "postgres",
+        type: 'postgres',
+        database: process.env.DB_USER || 'admin',
+        username: process.env.DB_USER || 'admin',
+        password: process.env.DB_PASSWORD || 'postgres',
         port: +(process.env.DB_PORT || 5432),
-        host: "db",
+        host: 'db',
         entities: entities,
         synchronize: true,
-        logger: "advanced-console",
-        logging: "all",
+        logger: 'advanced-console',
+        logging: 'all',
         dropSchema: true,
         cache: true,
         
