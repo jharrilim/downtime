1c9a4b8a5760a4490e8d9f561726cc9a55b97bff
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-04-11 22:13:13 -0400

    Fix date

--- client/src/util/time.ts
+++ client/src/util/time.ts
@@ -1,14 +1,14 @@
-import moment, { Moment } from 'moment';
+import moment from 'moment';
 
 export const traceOrDate = (date: Date) => {
 
     return function (thresholdHours: number = 24) {
-        if (moment().hours() - date.getHours() > thresholdHours)
-            return moment(date).format('MMMM Do YYYY, h:mm:ss a');
-        
         const now = moment();
         const d = moment(date);
         const hourDiff = now.diff(d, 'hours');
+        if (hourDiff > thresholdHours)
+            return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+        
         
         if (hourDiff > 0)
             return `${hourDiff} hours ago`;
