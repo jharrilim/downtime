739367413b20c357b6befce4d7c276357e343eed
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2019-04-15 21:46:27 -0400

    Run server in cluster

--- server/src/data/connection.ts
+++ server/src/data/connection.ts
@@ -4,7 +4,7 @@ import { User } from './entities/user';
 import { encryptPassword } from '../security';
 import { Role } from './entities/role';
 
-export async function connect(): Promise<Connection> {
+export async function connect(dropSchema: boolean = false): Promise<Connection> {
 
     if (process.env.DB_TYPE === 'mongodb') {
         return await createConnection({
@@ -15,10 +15,10 @@ export async function connect(): Promise<Connection> {
             port: +(process.env.DB_PORT || 27017),
             host: process.env.DB_HOST || 'mongodb',
             entities: entities,
-            synchronize: true,
+            synchronize: dropSchema,
             logger: 'advanced-console',
             logging: 'all',
-            dropSchema: true,
+            dropSchema,
             cache: true,
 
         });
@@ -32,13 +32,17 @@ export async function connect(): Promise<Connection> {
         port: +(process.env.DB_PORT || 5432),
         host: process.env.DB_HOST || 'localhost',
         entities: entities,
-        synchronize: true,
+        synchronize: process.env.NODE_ENV !== 'production' && dropSchema,
         logger: 'advanced-console',
         logging: 'all',
-        dropSchema: process.env.NODE_ENV !== 'production',
+        dropSchema: process.env.NODE_ENV !== 'production' && dropSchema,
         cache: true,
 
     });
+}
+
+export async function initialConnection() {
+    return await connect(true);
 }
 
 export async function seed() {
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,18 +2,20 @@ import 'reflect-metadata';
 import * as dotenv from 'dotenv';
 import { ApolloServer, ServerInfo } from 'apollo-server';
 import { Container } from 'typedi';
-import { useContainer, getRepository } from'typeorm';
+import { useContainer, getRepository, Connection } from 'typeorm';
 import { buildSchema } from "type-graphql";
 import { connect, seed } from './data/connection';
 import { Context } from './data/resolvers/types/context';
 import { User } from './data/entities/user';
 import { parseUserFromToken, authChecker } from './security';
+import { isMaster, fork, on as clusterOn } from 'cluster';
+import { cpus } from 'os';
+import { GraphQLSchema } from 'graphql';
 
-async function bootstrap(): Promise<ServerInfo> {
-    dotenv.config();
-    const port = +(process.env.PORT || 8080);
-    useContainer(Container);
-    await connect();
+dotenv.config();
+console.log('Starting...');
+
+async function initialize() {
     const schema = await buildSchema({
         resolvers: [`${__dirname}/data/resolvers/**/*.resolver.ts`],
         container: Container,
@@ -21,27 +23,80 @@ async function bootstrap(): Promise<ServerInfo> {
     });
     const { defaultUser } = await seed();
 
-    const server = new ApolloServer({ schema, context: async ({ req }): Promise<Context | null> => {
-        if (process.env.NODE_ENV !== 'production') {
-            return { user: defaultUser } as Context;
-        }
-        const token = req.headers.authorization;
-        if(token !== undefined) {
-            const userRepository = getRepository(User);
-            const userFromToken = await parseUserFromToken(token);
-            const user = await userRepository.findOne({ where: {
-                id: userFromToken.id,
-                email: userFromToken.email,
-                username: userFromToken.username
-            }});
-            return { user } as Context;
-        }
-        return null;
-    }});
+    return { schema, defaultUser };
+}
+
+async function bootstrap(schema: GraphQLSchema, defaultUser: User): Promise<ServerInfo> {
+    const port = +(process.env.PORT || 8080);
+    useContainer(Container);
+    await connect();
+
+    const server = new ApolloServer({
+        schema, context: async ({ req }): Promise<Context | null> => {
+            if (process.env.NODE_ENV !== 'production') {
+                return { user: defaultUser } as Context;
+            }
+            const token = req.headers.authorization;
+            if (token !== undefined) {
+                const userRepository = getRepository(User);
+                const userFromToken = await parseUserFromToken(token);
+                const user = await userRepository.findOne({
+                    where: {
+                        id: userFromToken.id,
+                        email: userFromToken.email,
+                        username: userFromToken.username
+                    }
+                });
+                return { user } as Context;
+            }
+            return null;
+        },
+    });
 
     return await server.listen(port);
 }
 
-bootstrap()
-    .then(({ url }) => console.log(`🚀  Server ready at ${url}`))
-    .catch(reason => console.error(reason));
+async function runMaster() {
+    if (process.env.NODE_ENV !== 'production') {
+        let conn: Connection | null = null;
+        try {
+            conn = await connect(true);
+        } finally {
+            if (conn)
+                await conn.close();
+        }
+    }
+    const workerCount = cpus().length;
+    console.log(`Creating ${workerCount} workers.`);
+
+    for (let i = 0; i < workerCount; i++) {
+        fork();
+    }
+
+    clusterOn('exit', worker => {
+        console.warn(`[${process.pid}] ${worker.id} died.`);
+        fork();
+    });
+}
+
+async function runWorker() {
+    let conn: Connection | null = null;
+    try {
+        conn = await connect();
+        const { defaultUser, schema } = await initialize();
+        bootstrap(schema, defaultUser)
+            .then(({ url }) => console.log(`🚀 [${process.pid}] Server ready at ${url}`))
+            .catch(reason => console.error(reason));
+    } finally {
+        if (conn)
+            await conn.close();
+    }
+}
+
+void async function main() {
+    if (isMaster) {
+        await runMaster();
+    } else {
+        await runWorker();
+    }
+}().catch(console.error);
