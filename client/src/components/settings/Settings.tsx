3c6a3748bcc5b960cdfa082810f978a780128269
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-04-17 08:17:27 -0400

    Add SettingsOptionList and styles

--- client/src/components/settings/Settings.tsx
+++ client/src/components/settings/Settings.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Typography, withStyles, Theme, createStyles } from '@material-ui/core';
+import { Typography, withStyles, Theme, createStyles, Grid } from '@material-ui/core';
+import { SettingsOptionsList } from './SettingsOptionsList';
 
 const styles = (theme: Theme) => createStyles({
   title: {
@@ -7,17 +8,33 @@ const styles = (theme: Theme) => createStyles({
   }
 });
 
+const mockSettings = [
+  'Profile',
+  'Security',
+  'Feedback',
+  'Others',
+
+]
+
 export const Settings = withStyles(styles)(() => {
   return (
-    <>
-      <Typography 
-        component="h5"
-        variant="h5"
-        color="inherit"
-        align="left"
-      >
-        Settings
-      </Typography>
-    </>
+    <Grid 
+      wrap="wrap"
+      container
+    >
+      <Grid item md={4}>
+        <Grid item>
+          <Typography 
+            component="h5"
+            variant="h5"
+            color="inherit"
+            align="left"
+          >      
+            Settings
+          </Typography>
+        </Grid>
+        <Grid item lg={6}><SettingsOptionsList labels={mockSettings} /></Grid>
+      </Grid>
+    </Grid>
   );
 });
---
+++ client/src/components/settings/SettingsOptionsList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { WithStyles, Theme, createStyles, withStyles, Divider, Typography, Grid } from '@material-ui/core';
+
+const styles = (theme: Theme) => createStyles({
+  label: {
+    paddingTop: '0.6em',
+    paddingBottom: '0.2em'
+  }
+});
+
+interface SettingsOptionsListPropTypes extends WithStyles<typeof styles> {
+  labels: string[];
+}
+
+const SettingsOptionsList = withStyles(styles)(({ labels, classes }: SettingsOptionsListPropTypes) => {
+  return (
+    <>
+      <nav>
+        {labels.map(label => (
+          <Grid wrap="wrap" container key={label}>
+            <Grid item lg={12}>
+              <Typography component="h5" variant="h5" className={classes.label}>{label}</Typography>
+            </Grid>
+            <Grid item lg={12}>
+              <Divider />
+            </Grid>
+          </Grid>
+        ))}
+      </nav>
+    </>
+  );
+});
+
+export { SettingsOptionsList };
