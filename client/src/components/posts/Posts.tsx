7f6c823d985e487f8e18925ceadc754feae81f97
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-04-18 17:19:16 -0400

    Refactor Posts, sort posts

--- client/src/components/posts/Posts.tsx
+++ client/src/components/posts/Posts.tsx
@@ -1,46 +1,49 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { Query, QueryResult } from 'react-apollo';
 import { Post } from './Post';
 import { traceOrDate } from '../../util/time';
 import { Typography, Grid } from '@material-ui/core';
 import { postsQuery } from '../../data/queries/posts';
 import { PostModel } from '../../data/models/Post.model';
+import moment from 'moment';
+import { OperationVariables } from 'apollo-boost';
 
 interface Response {
   posts: PostModel[];
 }
 
+const queryResultHandler = ({ loading, error, data }: QueryResult<Response, OperationVariables>): JSX.Element => {
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.error(error);
+    return <p>Error loading content.</p>;
+  }
+  if (data!.posts.length <= 0)
+    return (
+      <Typography variant="h4">
+        There are currently no posts yet. Click the post button on the left to be the first!
+      </Typography>
+    );
+
+  return (
+    <Grid container spacing={24}>
+      {data!.posts.sort((a, b) => -moment(a.dateCreated).diff(b.dateCreated)).map(({ content, author, dateCreated, title }) => (
+        <Grid item lg={6} sm={12}>
+          <Post
+            key={`${author.username}-${title}`}
+            content={content}
+            author={author.username}
+            title={title}
+            date={traceOrDate(new Date(dateCreated!))()} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
 const Posts = () => (
   <Query<Response> query={postsQuery} >
-    {({ loading, error, data }) => {
-
-      if (loading) return <p>Loading...</p>;
-      if (error) {
-        console.error(error);
-        return <p>Error loading content.</p>;
-      }
-      if (data!.posts.length <= 0)
-        return (
-          <Typography variant="headline">
-            There are currently no posts yet. Click the post button on the left to be the first!
-          </Typography>
-        );
-
-      return (
-        <Grid container spacing={24}>
-          {data!.posts.map(({ content, author, dateCreated, title }) => (
-              <Grid item lg={6} sm={12}>
-                <Post
-                  key={`${author.username}-${title}`}
-                  content={content}
-                  author={author.username}
-                  title={title}
-                  date={traceOrDate(new Date(dateCreated!))()} />
-              </Grid>
-          ))}
-        </Grid>
-      );
-    }}
+    {queryResultHandler}
   </Query>
 );
 
